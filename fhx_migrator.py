25c1f74309fa56a7391c9d14b983df93c3d2de56
mobosa <mobosa@users.noreply.github.com>
2026-05-26 15:53:10 +0800

    Add author info to GUI window

--- fhx_migrator.py
+++ fhx_migrator.py
@@ -2601,6 +2601,9 @@ class FHX_Migrator_App:
         nb.add(tab_cs, text=" Control Strategies ")
         self._build_tab3(tab_cs)
 
+        author_label = tk.Label(self.root, text="Author: Jared.Ji", fg="gray", font=("Arial", 9))
+        author_label.pack(side=tk.BOTTOM, pady=(0, 5))
+
     def _build_tab_setup(self, parent):
         frame = tk.Frame(parent, padx=10, pady=10)
         frame.pack(fill=tk.BOTH, expand=True)
